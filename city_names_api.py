82b66251c1d66bc8dc69a757433c36b6acfeeebb
vladimir pozov <vovasp@gmail.com>
2023-03-19 13:43:48 +0300

    chore: Update exception handle

--- city_names_api.py
+++ city_names_api.py
@@ -3,7 +3,6 @@ import requests
 
 from config import city_autocomplite_api_url, HEADERS
 from exceptions import CriticalExeption, NotCriticalExeption, NotFoundException
-from template_messages import CRITICAL_WARNING_MESSAGE, NON_CRITICAL_WARNING_MESSAGE
 
 
 def requests_handler(url):
@@ -14,12 +13,12 @@ def requests_handler(url):
         try:
             response = check_source.get(url, headers=HEADERS, params=None, timeout=1.0)
             response.raise_for_status()
-        except requests.exceptions.HTTPError:
-            return "not_critical_error"
-        except requests.exceptions.Timeout:
-            return "not_critical_error"
-        except requests.exceptions.RequestException:
-            return "critical_error"
+        except requests.exceptions.HTTPError as exc:
+            return ("not_critical_error", exc)
+        except requests.exceptions.Timeout as exc:
+            return ("not_critical_error", exc)
+        except requests.exceptions.RequestException as exc:
+            return ("critical_error", exc)
         else:
             return response
 
@@ -30,10 +29,10 @@ def check_on_valid_city_name(city_name):
     """
     url = city_autocomplite_api_url.format(city_name)
     api_response = requests_handler(url)
-    if api_response == "critical_error":
-        raise CriticalExeption(CRITICAL_WARNING_MESSAGE)
-    if api_response == "not_critical_error":
-        raise NotCriticalExeption(NON_CRITICAL_WARNING_MESSAGE)
+    if api_response[0] == "critical_error":
+        raise CriticalExeption(api_response[1])
+    if api_response[0] == "not_critical_error":
+        raise NotCriticalExeption(api_response[1])
     else:
         city_name_matches = api_response.json()
         if city_name_matches:
