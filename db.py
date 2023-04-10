c66899c45507bb901b857f1824ac519924f18cd9
Владимир Позов <vovasp@gmail.com>
2023-04-10 22:54:38 +0300

    feat: Add database execute and close methods

--- db.py
+++ db.py
@@ -6,3 +6,16 @@ import config
 def get_db():
     con = sqlite3.Connection(config.SQLITE_DB_FILE)
     return con
+
+
+def execute(sql, params):
+    db = get_db()
+    cur = db.cursor()
+    args = (sql, params)
+    cur.execute(*args)
+    db.commit()
+
+
+def close_db():
+    db = get_db()
+    db.close()
