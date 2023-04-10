c3f9b33cc78c7dd648d81bd35fc8a48fe4ae4da9
Владимир Позов <vovasp@gmail.com>
2023-04-10 23:01:41 +0300

    feat: Add request from api method

---
+++ user_data_api.py
@@ -0,0 +1,25 @@
+import requests
+
+from config import API_USERS_GET_URL, VK_BOT_TOKEN, HEADERS
+from exceptions import CriticalExeption, NotCriticalExeption
+
+
+def get_user(uid):
+    with requests.Session() as session:
+        try:
+            api_response = session.get(
+                API_USERS_GET_URL.format(uid, VK_BOT_TOKEN),
+                headers=HEADERS,
+                params=None,
+                timeout=5.0,
+            )
+            api_response.raise_for_status()
+        except requests.exceptions.HTTPError:
+            raise NotCriticalExeption
+        except requests.exceptions.Timeout:
+            raise NotCriticalExeption
+        except requests.exceptions.RequestException:
+            raise CriticalExeption
+        else:
+            api_response = api_response.json()
+            return api_response
