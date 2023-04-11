ce7434eb4dfae97d7fba1b7fffb4a31377b3034a
Владимир Позов <vovasp@gmail.com>
2023-04-11 11:55:43 +0300

    fix: Lost commas

--- db.sql
+++ db.sql
@@ -3,14 +3,14 @@ create table user (
   uid integer,
   first_name varchar(50),
   last_name varchar(50),
-  mobile_phone varchar(20),
+  mobile_phone varchar(20)
 );
 
 create table ticket (
   id integer primary key,
   departure_city varchar(20),
   destination_city varchar(20),
-  depart_date varchar(20)
+  depart_date varchar(20),
   user_id integer,
   foreign key (user_id) references user(id)
 );
