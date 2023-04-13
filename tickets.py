91daf979e0c61acbccfaead3563acef6952f9849
vladimir pozov <vovasp@gmail.com>
2023-04-13 13:00:40 +0300

    fx: Fix query param for execution

--- tickets.py
+++ tickets.py
@@ -3,7 +3,7 @@ from db import execute
 
 def insert_ticket(user_request):
     execute(
-        "INSERT INTO TABLE ticket (departure_city, destination_city, depart_date) VALUES (?, ?, ?)",
+        "INSERT INTO ticket (departure_city, destination_city, depart_date) VALUES (?, ?, ?)",
         (
             user_request["departure_city"],
             user_request["destination_city"],
