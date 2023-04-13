358a4f2fe10822100dc1180ea741694bbe062c56
vladimir pozov <vovasp@gmail.com>
2023-04-13 19:35:50 +0300

    fix: Add date of searching into query

--- tickets.py
+++ tickets.py
@@ -1,14 +1,18 @@
+from datetime import datetime
+
 from db import execute
 
 
 def insert_ticket(user_request):
     execute(
-        "INSERT INTO ticket (departure_city, destination_city, depart_date, user_id) VALUES \
-            (:departure_city, :destination_city, :depart_date, :user_id)",
+        "INSERT INTO ticket (departure_city, destination_city, depart_date, user_id, search_date) \
+            VALUES \
+            (:departure_city, :destination_city, :depart_date, :user_id, :search_date)",
         {
             "departure_city": user_request["departure_point"],
             "destination_city": user_request["arrival_point"],
             "depart_date": user_request["depart_at"],
-            "user_id": user_request["user_id"]
+            "user_id": user_request["user_id"],
+            "search_date": datetime.now().strftime('%Y-%m-%d')
         },
     )
