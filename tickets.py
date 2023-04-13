685f4c221bf35f9d072ff8e6bbe892d9424a3acc
vladimir pozov <vovasp@gmail.com>
2023-04-13 14:30:31 +0300

    refactor: Change params to dict

--- tickets.py
+++ tickets.py
@@ -3,10 +3,11 @@ from db import execute
 
 def insert_ticket(user_request):
     execute(
-        "INSERT INTO ticket (departure_city, destination_city, depart_date) VALUES (?, ?, ?)",
-        (
-            user_request["departure_city"],
-            user_request["destination_city"],
-            user_request["depart_date"],
-        ),
+        "INSERT INTO ticket (departure_city, destination_city, depart_date) VALUES \
+            (:departure_city, :destination_city, :depart_date)",
+        {
+            "departure_city": user_request["departure_point"],
+            "destination_city": user_request["arrival_point"],
+            "depart_date": user_request["depart_at"],
+        },
     )
