3474afa64fca2d9faffc49b0fe0e73091daaa893
vladimir pozov <vovasp@gmail.com>
2023-04-13 17:51:41 +0300

    fix: Add ignore into query

--- users.py
+++ users.py
@@ -15,6 +15,6 @@ def insert_user(uid):
             user_data["mobile_phone"],
         )
         execute(
-            "INSERT INTO user (uid, first_name, last_name, mobile_phone) VALUES (?, ?, ?, ?)",
+            "INSERT OR IGNORE INTO user (uid, first_name, last_name, mobile_phone) VALUES (?, ?, ?, ?)",
             personal_data,
         )
