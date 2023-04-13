f67d54852ee7fa517a6bfd9f3700a90b68c6a27c
vladimir pozov <vovasp@gmail.com>
2023-04-13 17:48:00 +0300

    fix: Add unique constraint to uid column and change byte size in varchar

--- db.sql
+++ db.sql
@@ -1,16 +1,16 @@
 create table user (
   id integer primary key,
-  uid integer,
-  first_name varchar(50),
-  last_name varchar(50),
-  mobile_phone varchar(20)
+  uid integer NOT NULL UNIQUE,
+  first_name varchar(255),
+  last_name varchar(255),
+  mobile_phone varchar(255)
 );
 
 create table ticket (
   id integer primary key,
-  departure_city varchar(20),
-  destination_city varchar(20),
-  depart_date varchar(20),
+  departure_city varchar(255),
+  destination_city varchar(255),
+  depart_date varchar(255),
   user_id integer,
   foreign key (user_id) references user(id)
 );
