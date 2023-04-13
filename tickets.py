4b42bce8ae56c4b1bb5d96f88e05c929912f0ec3
vladimir pozov <vovasp@gmail.com>
2023-04-13 18:44:27 +0300

    fix: Add user_id value in query

--- tickets.py
+++ tickets.py
@@ -3,11 +3,12 @@ from db import execute
 
 def insert_ticket(user_request):
     execute(
-        "INSERT INTO ticket (departure_city, destination_city, depart_date) VALUES \
-            (:departure_city, :destination_city, :depart_date)",
+        "INSERT INTO ticket (departure_city, destination_city, depart_date, user_id) VALUES \
+            (:departure_city, :destination_city, :depart_date, :user_id)",
         {
             "departure_city": user_request["departure_point"],
             "destination_city": user_request["arrival_point"],
             "depart_date": user_request["depart_at"],
+            "user_id": user_request["user_id"]
         },
     )
