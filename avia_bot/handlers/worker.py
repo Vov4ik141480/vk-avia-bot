5d5dd4aece9d9cdd4eb47d3a0ea3ca1feca8c58e
vladimir pozov <vovasp@gmail.com>
2025-02-23 15:58:51 +0300

    style: Delete empty spaces

--- avia_bot/handlers/worker.py
+++ avia_bot/handlers/worker.py
@@ -9,17 +9,16 @@ class Worker:
         self.events_queue = events_queue
         self.messages_queue = messages_queue
 
-
     async def _worker(self):
-        """Берет данные из очереди событий, извлекает данные 
+        """Берет данные из очереди событий, извлекает данные
         и ставит в очередь сообщений.
         """
         while True:
             if self.events_queue:
                 user_response = self.events_queue.pop()
-                user_id = user_response['from_id']
-                text = user_response['text'].lower()
-                Response = namedtuple("Response", "user_id text")
+                user_id = user_response["from_id"]
+                text = user_response["text"].lower()
+                Response = namedtuple("Response", ["user_id", "text"])
                 await self.messages_queue.put(Response(user_id, text))
             else:
                 await asyncio.sleep(0.1)
